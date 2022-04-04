5592f8f6903e3d9cf0b0ff2be8384b9d5f71aa59
Danial Azeem <dazeem.bscs18seecs@seecs.edu.pk>
2022-04-04 16:25:17 +0500

    Created controller for user and added mailer in the program

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "posts#index"
+  resources :users
+  root "users#create"
 
   resources :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_10_204107) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_04_111014) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "login"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
